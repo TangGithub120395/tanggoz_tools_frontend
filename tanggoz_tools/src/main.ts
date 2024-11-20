98abde24eddf96b31c0d1d2f81270c69d02d3b28
toby.tang <toby.tang@hisunglobal.com>
2024-11-20 17:00:56 +0800

    #task 导入element-plus

--- tanggoz_tools/src/main.ts
+++ tanggoz_tools/src/main.ts
@@ -1,6 +1,7 @@
 import './assets/main.css'
-
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
@@ -9,6 +10,7 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(ElementPlus)
 app.use(router)
 
 app.mount('#app')
